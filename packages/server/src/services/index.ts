e5af913668e6e84aad95ee7d9fb67b44806e1b54
trevor@ultra.io <trevor@ultra.io>
2023-04-10 10:28:11 -0600

    Finish Database Methods

--- packages/server/src/services/index.ts
+++ packages/server/src/services/index.ts
@@ -2,3 +2,4 @@ export * as discord from './discord';
 export * as database from './database';
 export * as express from './express';
 export * as messageProvider from './messageProvider';
+export * as blockchain from './blockchain';
